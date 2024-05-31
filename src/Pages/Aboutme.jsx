1a2ccc2553b7d8232322560936cdf9dea94fb46c
Moideen Mashad <moideenmashad@gmail.com>
2024-05-31 06:20:52 +0530

    Updated Allignement

--- src/Pages/Aboutme.jsx
+++ src/Pages/Aboutme.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../Assets/Theme/Style.css";
 import "./Main.css";
 import userData from "../Utils/user_data.json";
 
@@ -13,7 +14,11 @@ const Aboutme = () => {
               {/* <img src={userImg} alt="" className="img-fluid" /> */}
             </div>
             <div className="col-12 col-md-6 col-lg-6">
-              <h6 className="sub_title">About Me</h6>
+              <h6 className="sub_title mb-16">About Me</h6>
+              <p className="category_title mb-12">
+                {userData && userData.job_role}
+              </p>
+              <p className="para mb-8"></p>
             </div>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Routes/App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/js/bootstrap.bundle.min"; // Import Bootstrap JavaScript
+import "bootstrap/dist/js/bootstrap.bundle.min"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
