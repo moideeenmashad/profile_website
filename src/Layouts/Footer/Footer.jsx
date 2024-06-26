a9dbeba989e2b9e47d7547d2eb6c013620fec77a
Moideen Mashad <moideenmashad@gmail.com>
2024-06-26 20:33:05 +0530

    Updated footer

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Footer.css";
 import { Link as ScrollLink } from "react-scroll";
+import { FaLinkedinIn, FaStackOverflow, FaInstagram } from "react-icons/fa";
+import { IoLogoGithub, IoMail } from "react-icons/io5";
+import userSocialProfile from "@/Utils/user_social_media_link.json";
+const socialMediaIcons = {
+  linkedin: <FaLinkedinIn />,
+  github: <IoLogoGithub />,
+  email: <IoMail />,
+  stack_overflow: <FaStackOverflow />,
+  instagram: <FaInstagram />,
+};
 
 const Footer = () => {
   const navItems = [
@@ -15,7 +25,7 @@ const Footer = () => {
     <>
       <footer className="footer_bg" id="footer">
         <div className="container">
-          <div className="">
+          <div className="" style={{ border: "2px solid red" }}>
             <div className="row justify-content-center">
               {navItems.map((item, index) => (
                 <div className="col-2 col-md-2 col-lg-1" key={index}>
@@ -34,6 +44,22 @@ const Footer = () => {
               ))}
             </div>
             <hr />
+            {/* social links */}
+            <div className="d-flex justify-content-center align-items-center">
+              {userSocialProfile &&
+                userSocialProfile.map((item, index) => (
+                  <a
+                    key={index}
+                    href={item.LinkTo}
+                    className="social_media_icon align-items-center me-3"
+                    target="_blank"
+                    data-toggle="tooltip"
+                    title={item.title}
+                  >
+                    {socialMediaIcons[item.iconId]}
+                  </a>
+                ))}
+            </div>
           </div>
         </div>
       </footer>
