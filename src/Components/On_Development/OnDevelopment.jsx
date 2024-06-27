d1471426e54b0a16194e4e2d55a90529dd838c88
Moideen Mashad <moideenmashad@gmail.com>
2024-06-27 22:33:45 +0530

    Updated notice-container

--- src/Components/On_Development/OnDevelopment.jsx
+++ src/Components/On_Development/OnDevelopment.jsx
@@ -4,8 +4,8 @@ import Marquee from "react-fast-marquee";
 const OnDevelopment = () => {
   return (
     <>
-      <div className="container-fluid">
-        <Marquee className="text-center bg-light small p-1">
+      <div className="notice-container">
+        <Marquee className="text-center bg-light small p-1 text-dark">
           Please note that this website is currently under development. Some
           features may not be fully functional. Thank you for your
           understanding.
